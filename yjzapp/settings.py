7fbe9a84d0c5619be5de3206af32a6af0c1555be
jasyu <jasyu@jasyudeMacBook-Pro.local>
2018-04-24 23:35:58 +0800

    use mysql database

--- yjzapp/settings.py
+++ yjzapp/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'yjzapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "yjzapp",
+        'USER': 'root',
+        'PASSWORD':"nokia",
+        'HOST':"127.0.0.1"
     }
 }
 
@@ -119,5 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
